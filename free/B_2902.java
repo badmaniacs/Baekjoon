f2492cef7124e8e927357fe1d6bc03e375f52818
badmaniacs <gitadora@dgu.ac.kr>
2022-01-21 10:22:25 +0900

    2022-01-21

---
+++ free/B_2902.java
@@ -0,0 +1,18 @@
+package baekjoon.free;
+import java.io.*;
+
+
+public class B_2902 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] data = br.readLine().split("-");
+        for (int i=0;i<data.length;i++) {
+            String[] data2 = data[i].split("");
+            bw.write(data2[0]);
+        }
+        bw.flush();
+        bw.close();
+    }
+    
+}
