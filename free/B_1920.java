c980110265445e9473adcae211c5eb2dae74df64
badmaniacs <gitadora@dgu.ac.kr>
2022-02-05 19:15:44 +0900

    2022-02-05

--- free/B_1920.java
+++ free/B_1920.java
@@ -1,29 +1,42 @@
 package baekjoon.free;
 import java.io.*;
+import java.util.*;
 
 public class B_1920 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));       
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         String[] data = br.readLine().split(" ");
         int M = Integer.parseInt(br.readLine());
-        br.readLine().split(" ");
-        for (int i = 0; i<M; i++) {
-            for (int j = 0; i<N; i++) {
-                
-                    if (Integer.parseInt(data[i])==Integer.parseInt(data[j])) {
-                        bw.write("1");
-                        bw.newLine();
-                    }
-
-                bw.write("0");
-                bw.newLine(); 
-            }
-
-        }bw.flush();
+        String[] data2 = br.readLine().split(" ");
+        ArrayList<Integer> list = new ArrayList<Integer>(N);
+        for (String i : data) {
+            list.add(Integer.parseInt(i));
+        }
+        Collections.sort(list);
+        for (String i : data2) {
+            bw.write(String.format("%d%n", BynarySearch(list, Integer.parseInt(i))));
+        }
+        bw.flush();
         bw.close();
-
+    }    
+    static int BynarySearch (ArrayList<Integer> a, int key) {
+        int left = 0;
+        int right = a.size()-1;
+        do {
+            int center = (left+right)/2;
+            if (a.get(center)==key) {
+                return 1;
+            }
+            else if(a.get(center)<key) {
+                left = center+1;
+            }
+            else {
+                right = center-1;
+            }
+        }
+        while (left<=right);
+        return 0;
     }
-    
 }
