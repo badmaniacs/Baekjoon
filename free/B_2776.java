62c1283e31e3d61b681a9be3513ddd48462aea9c
badmaniacs <gitadora@dgu.ac.kr>
2022-02-23 18:01:49 +0900

    2022-02-23

---
+++ free/B_2776.java
@@ -0,0 +1,32 @@
+package baekjoon.free;
+import java.io.*;
+import java.util.*;
+
+public class B_2776{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        TreeSet<Integer> tree = new TreeSet<>();
+        for (int i=0;i<T;i++){
+            int N = Integer.parseInt(br.readLine());
+            String[] data1 = br.readLine().split(" ");
+            for(String a : data1){
+                tree.add(Integer.parseInt(a));
+            }
+            int M = Integer.parseInt(br.readLine());
+            String[] data2 = br.readLine().split(" ");
+            for(String b : data2){
+                if( tree.contains(Integer.parseInt(b))){
+                    bw.write("1\n");
+                }
+                else{
+                    bw.write("0\n");
+                }
+            }
+            tree.clear();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
