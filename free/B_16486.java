a94f4c0cc5a0a65a01444ccdb0db003bad5b8d55
badmaniacs <gitadora@dgu.ac.kr>
2022-03-02 21:46:39 +0900

    2022-03-02

---
+++ free/B_16486.java
@@ -0,0 +1,17 @@
+package baekjoon.free;
+
+import java.io.*;
+
+public class B_16486 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int dOne = Integer.parseInt(br.readLine());
+        int dTwo = Integer.parseInt(br.readLine());
+        double PI = 3.141592;
+        bw.write(String.format("%s", (dOne*2)+(2*dTwo*PI)));
+        bw.flush();
+
+    }
+    
+}
