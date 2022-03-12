cb942edada7c59250adb9d58ecb50a0cee1eb0e9
badmaniacs <gitadora@dgu.ac.kr>
2022-03-12 16:38:47 +0900

    2022-03-12

--- free/B_1978.java
+++ free/B_1978.java
@@ -1,17 +1,29 @@
 package baekjoon.free;
 
 import java.io.*;
-import java.util.LinkedList;
 
 public class B_1978 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        new BufferedWriter(new OutputStreamWriter(System.out));
-        Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
         String[] data = br.readLine().split(" ");
-        LinkedList<Integer> list = new LinkedList<Integer>();
-        for (String i : data) {
-            list.add(Integer.parseInt(i));
+        int number = 0;
+        for (int i=0;i<N;i++){
+            number += isPrimeNumber(Integer.parseInt(data[i]));
         }
+        bw.write(Integer.toString(number));
+        bw.flush();
+    }
+    static int isPrimeNumber(int x){
+        if (x==1){
+            return 0;
+        }
+        for (int i=2;i<x;i++){
+            if(x % i == 0){
+                return 0;
+            }
+        }
+        return 1;
     }
 }
---
+++ free/B_2581.java
@@ -0,0 +1,46 @@
+package baekjoon.free;
+
+import java.io.*;
+import java.util.*;
+
+import javax.print.FlavorException;
+
+public class B_2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int sum = 0;
+        ArrayList<Integer> List = new ArrayList<>();
+        for (int i = M ; i<=N;i++){
+            if(isPrimeNumber(i)){
+                List.add(i);
+            }
+        }
+        if(List.size()==0){
+            bw.write("-1");
+        }
+        else{
+            Iterator<Integer> it = List.iterator();
+            while(it.hasNext()){
+                sum += it.next();
+            }
+            bw.write(Integer.toString(sum));
+            bw.newLine();
+            bw.write(String.format("%d", Collections.min(List)));
+        }
+        bw.flush();
+    }
+    static boolean isPrimeNumber(int x){
+        if(x == 1){
+            return false;
+        }
+        for (int i=2; i<x; i++){
+            if(x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }  
+}
