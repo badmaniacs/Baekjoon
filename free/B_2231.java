92ebc5d7bf507abf71e30fa4652f1da7f7a71b22
badmaniacs <gitadora@dgu.ac.kr>
2022-03-15 14:00:10 +0900

    2022-03-15

--- free/B_2231.java
+++ free/B_2231.java
@@ -6,14 +6,20 @@ public class B_2231 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-    
-    bw.write(N);
-    bw.flush();
-    bw.close();
-        
-    }
-    static int Number(int N) {
-        int sum = N % 10;
-     return sum;
+        int C = 0;
+        int Con = 0;
+        for(int i=0;i<N;i++){
+            String[] data = Integer.toString(i).split("");
+            C = i;
+            for(int j=0;j<data.length;j++){
+                C += Integer.parseInt(data[j]);
+            }
+            if(C==N){
+                Con = i;
+                break;     
+            }
+        }
+        bw.write(Integer.toString(Con));  
+        bw.flush();
     }
 }
